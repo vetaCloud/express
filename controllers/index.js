6028b1749abd1f397f070a66f3d43685e2b109cb
Oladipo Adesiyan <adesiyanoladipo1@gmail.com>
2022-07-26 20:02:31 +0100

    Perf: Added try and catch

--- controllers/index.js
+++ controllers/index.js
@@ -7,102 +7,148 @@ exports.homepage = function(req, res, next) {
   res.render('index');
 }
 
-// exports.home = function(req, res, next){
-//   axios({
-//     maxContentLength: Infinity, maxBodyLength: Infinity, 
-//     headers: {'X_API_KEY': 'VTCD_PRIVATE_884b1fccfbd0882267636854bcddf1', 'X_ROUTE_NAME': "profile-picture"},
-//     method: 'post',
-//     url: 'http://localhost:3000',
-//     data: {
-//         file: req.files, raw: fs.readFileSync(req.files.file.path)
-//     }
-//   }).then(function (response) {
-//     fs.unlinkSync(req.files.file.path)
-//     if(response.status !== "error"){
-//       console.log(response.data)
-//     }
-//     else{
-//       console.log(response.data)
-//     }
-//     res.redirect('/')
-//   });
-// }
+exports.home = function(req, res, next){
+  try{
+     axios({
+      maxContentLength: Infinity, maxBodyLength: Infinity, 
+      headers: {'X_API_KEY': 'VTCD_PRIVATE_884b1fccfbd0882267636854bcddf1', 'X_ROUTE_NAME': "profile-picture"},
+      method: 'post',
+      url: 'http://localhost:3000',
+      data: {
+          file: req.files, raw: fs.readFileSync(req.files.file.path)
+      }
+    }).then(function (response) {
+      fs.unlinkSync(req.files.file.path)
+      if(response.status !== "error"){
+        console.log(response.data)
+      }
+      else{
+        console.log(response.data)
+      }
+      res.redirect('/')
+    }); 
+  }
+  catch(err){
+    return Response.send(
+      res,
+      500,
+      "Error occured"
+    )
+  }
+}
 
-/////UPLOAD FILE WITH A DEMAND! ----- IMAGE FILE
-// exports.demandImage = function(req, res, next){
-//   axios({
-//     headers: {'X_API_KEY': 'VTCD_PRIVATE_884b1fccfbd0882267636854bcddf1', 'X_ROUTE_NAME': "profile-picture"},
-//     method: 'post',
-//     url: 'http://localhost:3000/image/50/50/10',
-//     data: {
-//           file: req.files, raw: fs.readFileSync(req.files.file.path)
-//           }
-//   }).then(function (response) {
-//     if(response.status !== "error"){
-//       console.log(response.data)
-//     }
-//     else{
-//       console.log(response.data)
-//     }
-//     fs.unlinkSync(req.files.file.path)
-//     res.redirect('/')
-//   });
-// }
+///UPLOAD FILE WITH A DEMAND! ----- IMAGE FILE
+exports.demandImage = function(req, res, next){
+  try{
+   axios({
+    headers: {'X_API_KEY': 'VTCD_PRIVATE_884b1fccfbd0882267636854bcddf1', 'X_ROUTE_NAME': "profile-picture"},
+    method: 'post',
+    url: 'http://localhost:3000/image/50/50/10',
+    data: {
+          file: req.files, raw: fs.readFileSync(req.files.file.path)
+          }
+    }).then(function (response) {
+      if(response.status !== "error"){
+        console.log(response.data)
+      }
+      else{
+        console.log(response.data)
+      }
+      fs.unlinkSync(req.files.file.path)
+      res.redirect('/')
+    });   
+  }
+  catch(err){
+    return Response.send(
+      res,
+      500,
+      "Error occured"
+    )
+  }
+}
 
 
 /////UPLOAD FILE WITH A DEMAND!  ---- VIDEO FILE
 exports.demandVideo = function(req, res, next){
-  axios({
-    maxContentLength: Infinity, maxBodyLength: Infinity, 
-    headers: {'X_API_KEY': 'VTCD_PRIVATE_884b1fccfbd0882267636854bcddf1', 'X_ROUTE_NAME': "profile-picture"},
-    method: 'post',
-    url: 'http://localhost:3000/video/50/50/10',
-    data: {
-          file: req.files, raw: fs.readFileSync(req.files.file.path)
-          }
-  }).then(function (response) {
-    if(response.status !== "error"){
-      console.log(response.data)
-    }
-    else{
-      console.log(response.data)
-    }
-    fs.unlinkSync(req.files.file.path)
-    res.redirect('/')
-  });
+  try{
+    axios({
+      maxContentLength: Infinity, maxBodyLength: Infinity, 
+      headers: {'X_API_KEY': 'VTCD_PRIVATE_884b1fccfbd0882267636854bcddf1', 'X_ROUTE_NAME': "profile-picture"},
+      method: 'post',
+      url: 'http://localhost:3000/video/50/50/10',
+      data: {
+            file: req.files, raw: fs.readFileSync(req.files.file.path)
+            }
+    }).then(function (response) {
+      if(response.status !== "error"){
+        console.log(response.data)
+      }
+      else{
+        console.log(response.data)
+      }
+      fs.unlinkSync(req.files.file.path)
+      res.redirect('/')
+    });
+  }
+  catch(err){
+    return Response.send(
+      res,
+      500,
+      "Error occured"
+    )
+  }
 }
-// /////DELETE FILE
-// exports.deleteFile = function(req, res, next){
-//   axios({
-//     headers: {'X_API_KEY': 'VTCD_PRIVATE_2cd6793f8daa684155ceed6b2a9c01', 'X_ROUTE_NAME': "profile-picture", 'X_FILE_NAME': "IHNUBDUpl2facbgCu-BSp.png"},
-//     method: 'delete',
-//     url: 'https://cloud.vetacloud.com/',
-//   }).then(function (response) {
-//     if(response.status !== error){
-//       console.log(response.data)
-//       fs.unlinkSync(req.files.file.path)
-//     }
-//     else{
-//       console.log(response.data)
-//     }
-//     res.redirect('/')
-//   });
-// }
+
+/////DELETE FILE
+exports.deleteFile = function(req, res, next){
+  try{
+    axios({
+      headers: {'X_API_KEY': 'VTCD_PRIVATE_2cd6793f8daa684155ceed6b2a9c01', 'X_ROUTE_NAME': "profile-picture", 'X_FILE_NAME': "IHNUBDUpl2facbgCu-BSp.png"},
+      method: 'delete',
+      url: 'https://cloud.vetacloud.com/',
+    }).then(function (response) {
+      if(response.status !== error){
+        console.log(response.data)
+        fs.unlinkSync(req.files.file.path)
+      }
+      else{
+        console.log(response.data)
+      }
+      res.redirect('/')
+    });
+  }
+  catch(err){
+    return Response.send(
+      res,
+      500,
+      "Error occured"
+    )
+  }
+}
 
 
-// /////GET FILES IN A ROUTE
-// exports.files = function(req, res, next){
-//   axios({
-//     headers: {'X_API_KEY': 'VTCD_PRIVATE_2cd6793f8daa684155ceed6b2a9c01', 'X_ROUTE_NAME': "profile-picture"},
-//     method: 'get',
-//     url: 'https://cloud.vetacloud.com/files',
-//   }).then(function (response) {
-//     if(response.status !== error){
-//       console.log(response.data)
-//     }
-//     else{
-//       console.log(response.data)
-//     }
-//     res.redirect('/')
-//   });
-// }
+/////GET FILES IN A ROUTE
+exports.files = function(req, res, next){
+  try{
+    axios({
+      headers: {'X_API_KEY': 'VTCD_PRIVATE_2cd6793f8daa684155ceed6b2a9c01', 'X_ROUTE_NAME': "profile-picture"},
+      method: 'get',
+      url: 'https://cloud.vetacloud.com/files',
+    }).then(function (response) {
+      if(response.status !== error){
+        console.log(response.data)
+      }
+      else{
+        console.log(response.data)
+      }
+      res.redirect('/')
+    });
+  }
+  catch(err){
+    return Response.send(
+      res,
+      500,
+      "Error occured"
+    )
+  }
+}
